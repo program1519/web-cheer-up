bbc0e22e90ceb824bb4c74c630fc174b17385d6e
program1519 <109532636+program1519@users.noreply.github.com>
2022-10-12 15:28:18 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+var name = Math.floor(Math.random() * 13);
+console.log(name)
+ what();
+ function what(){
+if (name == 1 ) {
+document.getElementById('c').innerHTML = "ขอให้เป็นวันที่ดี :)"	
+} 
+else if (name == 2 ) {
+	document.getElementById('c').innerHTML = "ความท้าทายทำให้เราเข้มแข็ง"	
+}
+else if (name == 3 ) {
+	document.getElementById('c').innerHTML = "บางปัญหาย่อมมีทางออก"	
+}
+else if (name == 5 ) {
+	document.getElementById('c').innerHTML = " เดี๋ยวมันก็ผ่านไป... อดทนหน่อยนะ...นะ"	
+}
+else if (name == 6 ) {
+	document.getElementById('c').innerHTML = "ชีวิตมีขึ้นมีลง.....วันนนี้ ลงใช่ป่ะ ลงมาเลยลงมาให้สุดๆ   เดี๋ยวพรุ่งนี้ จะได้ขึ้นแบบสุดๆเหมือนกัน!!!"	
+}
+else if (name == 7 ) {
+	document.getElementById('c').innerHTML = "เราทุกคนล้วนมีปัญหาด้วยกันทั้งนั้น ปัญหาที่เราเจอในวันนี้ที่เราคิดว่าเป็นเรื่องใหญ่สำหรับเรา ตอนนี้บนโลกนี้อาจกำลังมีคนที่กำลังประสบปัญหาที่หนักกว่าเราอยู่ก็เป็นได้"	
+}
+else if (name == 8 ) {
+	document.getElementById('c').innerHTML = "มีคนที่แย่กว่าเราอีกเยอะ..."	
+}
+else if (name == 9 ) {
+	document.getElementById('c').innerHTML = "ทุกๆ เช้าวันใหม่ จะมาพร้อมกับโอกาส ให้เราเริ่มต้นอีกครั้ง"	
+}
+else if (name == 10 ) {
+	document.getElementById('c').innerHTML = "ข้างหน้ายังมีเรื่องให้ท้อใจอีกเยอะ :o "	
+}
+else if (name == 11 ) {
+	document.getElementById('c').innerHTML = "ทุกอย่างที่เกิดขึ้น มันอาจเกิดจากตัวเรา ที่ทำลงไปแล้ว ดังนั้นก็ต้องยอมรับผลที่ตามมา ไม่ว่าจะดีหรือร้าย ไม่มีใครแย่ตลอดชีวิต หรือดีตลอดชีวิต มันก็ต้องมีปัญหากันทุกคน  ต้องอยู่ให้ได้ ชีวิตต้องดำเนินต่อไป ถ้าใจเราไม่สู้ ใครก็ช่วยเราไม่ได้หรอก เราต้องมีสติ ถ้ามันไม่ไหวจริง ๆ ก็ยอมแพ้ และ หยดอยู่นิ่ง ๆ ให้เหตุการณ์มันคลี่คลายไปเอง..."	
+}
+else if (name == 12 ) {
+	document.getElementById('c').innerHTML = "บางปัญหาเรารับไม่ไว้อย่า ฆ่าตัวตาย เลยน่า สุ้ๆหน่อยหนอะ เดี๋ยวมันก็ผ่านไป "	
+}
+else if (name == 13 ) {
+	document.getElementById('c').innerHTML = "เกิดปัญหา?  1. หาสาเหตุ 2.หาวิธิแก้ 3.แก้ปัญหา  เห็นไหมแค่นี้เอง ถึงมันจะเยอะแค่ไหนก็พยามเข้านะ!"	
+}
+else {
+	document.getElementById('c').innerHTML = "เรื่องบางเรื่องที่เคยแพ้ ก็ไม่ได้แปลว่าเราจะต้องแพ้ตลอดไป"	
+}
+ };
